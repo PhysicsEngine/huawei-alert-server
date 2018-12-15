f3d719069d45b9802d0c21e61315a61770b2edb1
Lewuathe <lewuathe@me.com>
2018-12-15 14:25:42 +0900

    Default notitication to Slack

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"go.uber.org/zap"
 	"log"
 	"net/http"
+	"bytes"	
 	"os"
 )
 
@@ -35,6 +36,32 @@ func main() {
 
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	})
+
+	router.POST("/api/notification", func(c *gin.Context) {
+		// TODO: Call plugin with parameter
+		jsonStr := "{}"
+		url := "https://maker.ifttt.com/trigger/huawei_alert/with/key/c9GxSBX5gGyKITjQTGsuwH"
+    req, err := http.NewRequest(
+			"POST",
+			url,
+			bytes.NewBuffer([]byte(jsonStr)),
+		)		
+		if err != nil {
+			logger.Errorf("Invalid http request")
+		}
+    req.Header.Set("Content-Type", "application/json")
+
+    client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			logger.Errorf("Fail to send notification")
+		}		
+    defer resp.Body.Close()		
+
+		c.JSON(200, gin.H{
+			"status": "OK",
+		})
 	})
 
 	router.Run(":" + port)
